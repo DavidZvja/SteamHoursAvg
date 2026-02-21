dd46cec9aff6e42e5db25fe26228131cc7138fa1
DavidTheBrain <david.zvjagin01@web.de>
2026-02-21 13:57:45 +0100

    Add files via upload

--- Steam hours avg/Calculate.java
+++ Steam hours avg/Calculate.java
@@ -13,4 +13,16 @@ public class Calculate
       
                     return avg;                    
           }
+
+          public static double ArraySum(double numbers[])
+          {
+                    double sum=0;          
+
+                    for (int i=0; i<numbers.length; i++)
+                    {
+                              sum=numbers[i]+sum;          
+                    }
+
+                    return sum;
+          }
 }
